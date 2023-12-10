f5e5357a0879cca9342b3a97c98b4d51b5af6625
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2023-12-10 14:21:21 +0530

    Add files via upload

---
+++ Finding two strings are equal or not.py
@@ -0,0 +1,8 @@
+
+print("---Finding whther two strings are ewual or not")
+first = input("Enterr a string")
+second = input("Enter a string two")
+if(first == second):
+    print(" >>> THE STRINGS ARE EQUAL")
+else:
+    print(" >>> THE STRINGS ARE NOT EQUAL")
